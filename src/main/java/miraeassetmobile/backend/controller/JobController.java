0da9e2b4092ab36a906ad3bc13be86b9299c358c
yujeong <dldbwjd24@gmail.com>
2023-02-21 08:04:39 +0900

    chore : 주석수정

--- src/main/java/miraeassetmobile/backend/controller/JobController.java
+++ src/main/java/miraeassetmobile/backend/controller/JobController.java
@@ -3,10 +3,7 @@ package miraeassetmobile.backend.controller;
 import miraeassetmobile.backend.domain.dto.jobs.JobListDto;
 import miraeassetmobile.backend.service.JobService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 //pagenation을 위한 import
@@ -25,15 +22,17 @@ public class JobController {
 
 
     //선생님 직업조회 페이지 -> 해당 학급에 모든 직업을 조회
-    //특정 클래스에
+    //특정 학급에서 생성된 전체 직업 과 공통 직업에 대한 정보를 page에 따라 10개씩 조회
     @GetMapping("/class")
     public ResponseEntity<JobListDto> jobListByClass(@RequestParam(value = "class_id")int classId,
                                                      @RequestParam(value="page", defaultValue = "0") int page){
 
-
         return ResponseEntity.ok(jobService.getJobListByClass(classId,page));
-
     }
+
+
+//    @DeleteMapping("/deletejob")
+//    public
 
 
 }
