6689fc6643faf658696ba6b5269901cc01aa8ffa
yujeong <dldbwjd24@gmail.com>
2023-02-21 10:54:08 +0900

    feat (job dto) : add job dto

---
+++ src/main/java/miraeassetmobile/backend/domain/dto/jobs/JobDto.java
@@ -0,0 +1,34 @@
+package miraeassetmobile.backend.domain.dto.jobs;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
+
+@Getter
+@Setter
+@Builder
+public class JobDto {
+
+    Long id;
+
+    private String title; //직업명
+
+    private String detail;
+
+
+    private int monthlySalary;
+
+    private int creditLimit;
+    private boolean isWithdrawStudent;
+
+    private boolean isWithdrawClass;
+
+    private boolean isModifyCredit;
+
+
+}
