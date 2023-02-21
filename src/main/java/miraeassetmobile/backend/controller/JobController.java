c615c77d78572c8e81d06bf6630c8edc739fa6e4
yujeong <dldbwjd24@gmail.com>
2023-02-21 10:53:50 +0900

    chore : requestparam -> pathvariable로 수정

--- src/main/java/miraeassetmobile/backend/controller/JobController.java
+++ src/main/java/miraeassetmobile/backend/controller/JobController.java
@@ -1,5 +1,6 @@
 package miraeassetmobile.backend.controller;
 
+import miraeassetmobile.backend.domain.dto.jobs.JobDto;
 import miraeassetmobile.backend.domain.dto.jobs.JobListDto;
 import miraeassetmobile.backend.service.JobService;
 import org.springframework.http.ResponseEntity;
@@ -23,16 +24,20 @@ public class JobController {
 
     //선생님 직업조회 페이지 -> 해당 학급에 모든 직업을 조회
     //특정 학급에서 생성된 전체 직업 과 공통 직업에 대한 정보를 page에 따라 10개씩 조회
-    @GetMapping("/class")
-    public ResponseEntity<JobListDto> jobListByClass(@RequestParam(value = "class_id")int classId,
+    @GetMapping("/class/{class_id}")
+    public ResponseEntity<JobListDto> jobListByClass(@PathVariable(value = "class_id")Long classId,
                                                      @RequestParam(value="page", defaultValue = "0") int page){
 
         return ResponseEntity.ok(jobService.getJobListByClass(classId,page));
     }
 
 
-//    @DeleteMapping("/deletejob")
-//    public
+    //job id를 통한 직업조회
+    //특정 직업의 정보를 넘겨줌 (직업 수정 화면 등등)
+    @GetMapping("/{job_id}")
+    public ResponseEntity<JobDto> getJobInfo(@PathVariable(value = "job_id") Long jobId){
+        return ResponseEntity.ok(jobService.getJobInfo(jobId));
+    }
 
 
     @DeleteMapping("/{job_id}/delete")
