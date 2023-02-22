c425b28ca644238adcc83fd0b773842b17e7370b
yujeong <dldbwjd24@gmail.com>
2023-02-22 16:46:45 +0900

    chore : 주석변경

--- src/main/java/miraeassetmobile/backend/service/StudentService.java
+++ src/main/java/miraeassetmobile/backend/service/StudentService.java
@@ -1,16 +1,9 @@
 package miraeassetmobile.backend.service;
 
 
-import miraeassetmobile.backend.domain.dto.students.StudentJobDto;
-import miraeassetmobile.backend.domain.entity.Job;
-import miraeassetmobile.backend.domain.entity.Student;
 import miraeassetmobile.backend.repository.JobRepository;
 import miraeassetmobile.backend.repository.StudentRepository;
 import org.springframework.stereotype.Service;
-
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class StudentService {
