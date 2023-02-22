65eca9acc55a7a08aa9890415c62bb3e6d0a8604
yujeong <dldbwjd24@gmail.com>
2023-02-22 16:46:31 +0900

    feat(student) : repository findbyId

--- src/main/java/miraeassetmobile/backend/repository/StudentRepository.java
+++ src/main/java/miraeassetmobile/backend/repository/StudentRepository.java
@@ -5,11 +5,16 @@ import miraeassetmobile.backend.domain.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     List<Student> findByClassId(Long classId);
 
+
+    Optional<Student> findById(Long id);
+
+    boolean existsById(Long id);
 
 
 }
