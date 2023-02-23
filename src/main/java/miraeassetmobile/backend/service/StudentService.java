796e88442d10f608caf7497a18e3cb004592fa56
yujeong <dldbwjd24@gmail.com>
2023-02-23 15:08:39 +0900

    feat(student-job-card) : 학생 직업 카드 정보 api

--- src/main/java/miraeassetmobile/backend/service/StudentService.java
+++ src/main/java/miraeassetmobile/backend/service/StudentService.java
@@ -2,7 +2,10 @@ package miraeassetmobile.backend.service;
 
 
 import miraeassetmobile.backend.domain.dto.students.StudentAccountResponseDto;
+import miraeassetmobile.backend.domain.dto.students.StudentJobResponseDto;
+import miraeassetmobile.backend.domain.dto.students.StudentTransferSelectorResponseDto;
 import miraeassetmobile.backend.domain.entity.Classes;
+import miraeassetmobile.backend.domain.entity.Job;
 import miraeassetmobile.backend.domain.entity.Student;
 import miraeassetmobile.backend.repository.ClassRepository;
 import miraeassetmobile.backend.repository.JobRepository;
@@ -10,6 +13,9 @@ import miraeassetmobile.backend.repository.StudentRepository;
 
 
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -44,6 +50,26 @@ public class StudentService {
     }
 
 
+    public StudentJobResponseDto getStudentJobInfo(Long studentId) {
+
+        Student student = studentRepository.findById(studentId).get();
+
+        Job studentJob = jobRepository.findById(student.getJobId()).get();
+
+        Classes studentClass = classRepository.findById(student.getClassId()).get();
+
+        String classInfo = studentClass.getGrade() + "학년 " + studentClass.getClassNum() + "반";
+
+
+        return StudentJobResponseDto.builder()
+                .studentId(student.getId())
+                .classInfo(classInfo)
+                .jobId(student.getJobId())
+                .JobTitle(studentJob.getTitle())
+                .JobDetail(studentJob.getDetail())
+                .isTransfer(studentJob.isWithdrawStudent()) // 이체하기 -> 학생계좌 출금
+                .isPay(studentJob.isWithdrawClass()) //지급하기 -> 국고 출금
+                .build();
 
 
 }
