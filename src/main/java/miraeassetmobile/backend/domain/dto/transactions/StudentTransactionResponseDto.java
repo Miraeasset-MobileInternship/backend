36d1432aa94742723451aecc08c1bb585b957e24
yujeong <dldbwjd24@gmail.com>
2023-02-23 14:02:33 +0900

    feat(add dto) : 학생 계좌 거래 내역 조회 response dto

---
+++ src/main/java/miraeassetmobile/backend/domain/dto/transactions/StudentTransactionResponseDto.java
@@ -0,0 +1,30 @@
+package miraeassetmobile.backend.domain.dto.transactions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class StudentTransactionResponseDto {
+
+    Long transactionId; //거래 아이디
+
+    //거래일
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+    private LocalDate transactionDate;
+
+    String category; //카테고리
+
+    String detail; //비고
+
+    boolean isDeposit; //입금:true / 출금:false
+
+    int transactionMoney; //거래금액
+
+
+
+
+}
