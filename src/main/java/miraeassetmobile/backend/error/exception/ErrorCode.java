a16a031b7a34e070e3f5b7f3e61604959aa7a32c
yujeong <dldbwjd24@gmail.com>
2023-02-24 13:37:58 +0900

    feat( 지급하기 기능 ) : 국고 잔고 부족 error 처리

--- src/main/java/miraeassetmobile/backend/error/exception/ErrorCode.java
+++ src/main/java/miraeassetmobile/backend/error/exception/ErrorCode.java
@@ -15,7 +15,10 @@ public enum ErrorCode {
     //401 : UNAUTHORIZED : 클라이언트는 접근 권리를 가지고 있지 않습니다. (클라이언트를 모르기 때문에 접근권한이 없음)
     UNAVAILABLE_ACTION_DELETE_JOB(HttpStatus.FORBIDDEN, "삭제 불가능한 직업입니다."),
     UNAVAILABLE_ACTION_TRANSFER_MONEY(HttpStatus.FORBIDDEN, "학생 잔고가 부족하여 돈을 이체할 수 없습니다."),
+    UNAVAILABLE_ACTION_PAY_MONEY(HttpStatus.FORBIDDEN, "국고가 부족하여 돈을 이체할 수 없습니다."),
     UNAVAILABLE_ACTION_JOB_TRANSFER(HttpStatus.FORBIDDEN, "학생 잔고를 이체할 수 있는 권한이 없는 직업입니다."),
+    UNAVAILABLE_ACTION_JOB_PAY(HttpStatus.FORBIDDEN, "국고를 출금할 수 있는 권한이 없는 직업입니다."),
+
 
 
     //존재하지 않는 리소스에 대한요청
