116257c8f9db747320bca718380594a74e55c9d0
yujeong <dldbwjd24@gmail.com>
2023-03-06 14:19:58 +0900

    chore

--- src/main/java/miraeassetmobile/backend/controller/StockController.java
+++ src/main/java/miraeassetmobile/backend/controller/StockController.java
@@ -2,12 +2,15 @@ package miraeassetmobile.backend.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import miraeassetmobile.backend.domain.dto.api.StockApiResponseDto;
+import miraeassetmobile.backend.domain.dto.stocks.StudentStockInfoResponseDto;
 import miraeassetmobile.backend.domain.dto.stocks.TotalStockInfoResponseDto;
 import miraeassetmobile.backend.domain.dto.students.StudentAccountResponseDto;
 import miraeassetmobile.backend.service.StockApiCallService;
 import miraeassetmobile.backend.service.StockService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/stock")
 @RestController
@@ -29,6 +32,12 @@ public class StockController {
     }
 
 
+
+//    @GetMapping("/student/{student_id}")
+//    @Operation(description = "보유 주식 종목별 정보, 학생 - 내 주식페이지 하단 보유 주식리스트 부분")
+//    public ResponseEntity<List<StudentStockInfoResponseDto>> getStudentAccountInfo(@PathVariable(value = "student_id") Long studentId){
+//        return ResponseEntity.ok(stockService.getStudentStockList(studentId)); //api 에서는 1페이지 부턴데 우리는 0페이지부터로 합의함
+//    }
 
 
 
