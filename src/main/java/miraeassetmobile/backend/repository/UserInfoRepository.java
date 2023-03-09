facde2bb36e0f3af28deed9ee0db3de446440349
yujeong <dldbwjd24@gmail.com>
2023-03-09 13:32:23 +0900

    chore : user->userInfo 이름변경

--- src/main/java/miraeassetmobile/backend/repository/UserInfoRepository.java
+++ src/main/java/miraeassetmobile/backend/repository/UserInfoRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<UserInfo,Long> {
+public interface UserInfoRepository extends JpaRepository<UserInfo,Long> {
 
     boolean existsByPhoneNum(String phoneNum);
 
     Optional<UserInfo> findByPhoneNum(String phoneNum);
+    Optional<UserInfo> findById(Long id);
+
 
 }
