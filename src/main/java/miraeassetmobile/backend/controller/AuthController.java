cfcd553e1680f77b93c179d9942c1230c82d3c16
yujeong <dldbwjd24@gmail.com>
2023-03-09 13:36:12 +0900

    feat(signup/in) : 로그인 및 회원가입 관련 controller 및 service

--- src/main/java/miraeassetmobile/backend/controller/AuthController.java
+++ src/main/java/miraeassetmobile/backend/controller/AuthController.java
@@ -7,6 +7,7 @@ package miraeassetmobile.backend.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import miraeassetmobile.backend.domain.dto.auth.SignInRequestDto;
 import miraeassetmobile.backend.domain.dto.auth.SignUpRequestDto;
+import miraeassetmobile.backend.domain.entity.UserInfo;
 import miraeassetmobile.backend.service.auth.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,26 +20,28 @@ public class AuthController {
 
     AuthService authService;
 
-    AuthController(AuthService authService){
-        this.authService=authService;
+    public AuthController(AuthService authService){
+        this.authService = authService;
     }
 
 
 
-    @PostMapping("/start")
-    @Operation(description = "시작하기 ")
+    @PostMapping("/start-signin")
+    @Operation(description = "시작하기/로그인 - 가입진행된 유저일 경우(바로 로그인됨), 가입안된 유저의 경우(303 : 가입진행창으로 넘어가게)")
     public ResponseEntity getStartWithSignIn(@RequestBody @Valid SignInRequestDto signInRequestDto){
-        System.out.println(signInRequestDto.getPhoneNumber());
         return ResponseEntity.ok(authService.getStart(signInRequestDto));
     }
 
 
-    @PostMapping("/signup")
-    @Operation(description = "회원 가입부터 시작하기 ")
+    @PostMapping("/start-signup")
+    @Operation(description = "회원가입 및 자동로그인")
     public ResponseEntity getStartWithSignUp(@RequestBody @Valid SignUpRequestDto signUpRequestDto){
         return ResponseEntity.ok(authService.startWithSignUp(signUpRequestDto));
 
     }
+
+
+
 
 
 }
--- src/main/java/miraeassetmobile/backend/service/auth/AuthService.java
+++ src/main/java/miraeassetmobile/backend/service/auth/AuthService.java
@@ -1,102 +1,126 @@
 package miraeassetmobile.backend.service.auth;
 
-import lombok.RequiredArgsConstructor;
 import miraeassetmobile.backend.config.security.jwt.TokenProvider;
-import miraeassetmobile.backend.domain.dto.auth.*;
+import miraeassetmobile.backend.domain.dto.auth.SignInRequestDto;
 import miraeassetmobile.backend.domain.dto.auth.token.RefreshToken;
 import miraeassetmobile.backend.domain.dto.auth.token.TokenDto;
 import miraeassetmobile.backend.domain.entity.Classes;
 import miraeassetmobile.backend.domain.entity.Student;
 import miraeassetmobile.backend.domain.entity.UserInfo;
-import miraeassetmobile.backend.error.exception.ErrorCode;
-import miraeassetmobile.backend.error.exception.NotExistException;
-import miraeassetmobile.backend.repository.ClassRepository;
-import miraeassetmobile.backend.repository.redis.RefreshTokenRedisRepository;
-import miraeassetmobile.backend.service.ErrorService;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 import miraeassetmobile.backend.domain.enums.UriTypes;
 import miraeassetmobile.backend.domain.enums.UserTypes;
+import miraeassetmobile.backend.error.exception.CustomLoginException;
+import miraeassetmobile.backend.error.exception.ErrorCode;
+import miraeassetmobile.backend.error.exception.NotExistException;
+import miraeassetmobile.backend.repository.ClassRepository;
 import miraeassetmobile.backend.repository.StudentRepository;
-import miraeassetmobile.backend.repository.UserRepository;
+import miraeassetmobile.backend.repository.UserInfoRepository;
+
+import miraeassetmobile.backend.domain.dto.auth.SignInResponseDto;
+import miraeassetmobile.backend.domain.dto.auth.SignUpRequestDto;
+import miraeassetmobile.backend.domain.dto.auth.AccessTokenInfo;
+import miraeassetmobile.backend.domain.dto.auth.ClassOnboardInfo;
+import miraeassetmobile.backend.domain.dto.auth.UserOnboardInfo;
+
+import miraeassetmobile.backend.repository.redis.RefreshTokenRedisRepository;
+import miraeassetmobile.backend.service.ErrorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.transaction.Transactional;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
 @Service
-@RequiredArgsConstructor
 public class AuthService {
+
 
 
     AuthenticationManagerBuilder authenticationManagerBuilder;
     ErrorService errorService;
-
-    UserRepository userRepository;
-
-    TokenProvider tokenProvider;
-    RefreshTokenRedisRepository refreshTokenRedisRepository;
+    UserInfoRepository userInfoRepository;
     ClassRepository classRepository;
-
     StudentRepository studentRepository;
+    TokenProvider tokenProvider;
+
+    RefreshTokenRedisRepository refreshTokenRedisRepository;
 
 
-
-    //2가지 리턴 경우의 수가 존재함..
-    public Object getStart(SignInRequestDto signInRequestDto){
-//        가입안된 유저임 -> 303 SEE OTHER return
-        if(!userRepository.existsByPhoneNum(signInRequestDto.getPhoneNumber())){
-            //핸드폰 번호 포함-가입을 다시 진행하라는 의미
-            return ResponseEntity.accepted().body(signInRequestDto.getPhoneNumber());
-        }
-
-
-        //가입된 유저임(로그인)
-        TokenDto t = login(signInRequestDto.getPhoneNumber());
-
-        return createSignInInfo(t);
-
-
+    AuthService(RefreshTokenRedisRepository refreshTokenRedisRepository,AuthenticationManagerBuilder authenticationManagerBuilder,TokenProvider tokenProvider, UserInfoRepository userInfoRepository, ErrorService errorService, ClassRepository classRepository, StudentRepository studentRepository){
+        this.userInfoRepository =userInfoRepository;
+        this.errorService=errorService;
+        this.classRepository=classRepository;
+        this.studentRepository=studentRepository;
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder=authenticationManagerBuilder;
+        this.refreshTokenRedisRepository = refreshTokenRedisRepository;
     }
 
 
 
+    public UserInfo getUser(Long userId){
+
+        UserInfo u = userInfoRepository.findById(userId).get();
+
+        System.out.println(u.getUserName());
+
+        return u;
+    }
+
+    public UserInfo getUserByPhone(String phoneNumber){
+        return userInfoRepository.findByPhoneNum(phoneNumber).get();
+    }
+
+
+    //2가지 리턴 경우의 수가 존재함..
+    @Transactional
+    public SignInResponseDto getStart(SignInRequestDto signInRequestDto){
+//        가입안된 유저임 -> 303 SEE OTHER return
+        if(!userInfoRepository.existsByPhoneNum(signInRequestDto.getPhoneNum())){
+            //핸드폰 번호 포함-가입을 다시 진행하라는 의미
+            throw new CustomLoginException(ErrorCode.SIGN_UP_REQUIRED); //이게안된다
+        }
+
+
+        //가입된 유저인 경우(로그인)
+        TokenDto t = login(signInRequestDto.getPhoneNum()); //이건잘딤
+
+        return createSignInInfo(t);
+
+    }
+
+
+    @Transactional
     public SignInResponseDto startWithSignUp(SignUpRequestDto signUpRequestDto){
 
 
         //1. 가입시키기
-        System.out.println(1);
-        System.out.println(signUpRequestDto.getPhoneNumber());
-        System.out.println(signUpRequestDto.getUserName());
-        System.out.println(signUpRequestDto.getUserRole());
 
-        System.out.println(11);
         //1) 가입 정보가 있는 유저인지 확인
-        errorService.alreadyExistUser(signUpRequestDto.getPhoneNumber());
+        errorService.alreadyExistUser(signUpRequestDto.getPhoneNum());
 
-
-        System.out.println(2);
 
         //2) 가입진행
-//        signUp(signUpRequestDto);
+        signUp(signUpRequestDto);
 
-
-        System.out.println(3);
 
         //2. 로그인
-//        TokenDto t = login(signUpRequestDto.getPhoneNumber());
+        TokenDto t = login(signUpRequestDto.getPhoneNum());
 
-        System.out.println(4);
 
         //3. 필요정보 수집
-//        return createSignInInfo(t);
+        return createSignInInfo(t);
 
-        return null;
+
+//        return null;
         /*
         필요정보
 
@@ -124,23 +148,43 @@ public class AuthService {
 
 
     //폰 번호로 로그인 시키기
+    @Transactional
     public TokenDto login(String phoneNumber){
 
-        //1. 다른 방식으로 인증했으므로 비밀번호 필요 없음
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(phoneNumber,null);
 
-        //2. 유효한 유저인지 검증
-        Authentication authentication = authenticationManagerBuilder.getObject()
-                .authenticate(authenticationToken);
+
+        //1. 다른 방식으로 인증했으므로 비밀번호 필요 없음
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(phoneNumber,"", AuthorityUtils.createAuthorityList("USER"));
+        //미인증 객체인 UserPasswordAuthenticationToken생성
+
+
+        //미인증 객체를 받아
+        //2. 유효한 유저인지 검증 -> 여기서 검증막힌다 유효하지 않다고 나옴
+        /*
+        에러원인 : 비밀번호를 사용하지 않아서 "" 를 넣었는데, (null로는안된더..)
+        자동으로 passwordEncoder를 시큐리티가 내장하고 있어서 비번이 맞지 않았음 -> userdetailservice 에 있는 password와 위의 credential이 일치해야함
+
+        그래서 encoder를 ㅏㅅ용하지 않는 설정을 security config에 넣어줌
+
+         */
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
 
         // 3. token 생성
         TokenDto tokenDto = tokenProvider.generateToken(authentication);
         // 4. refresh token 생성 ( database 및 redis 저장을 위한 refresh token )
+
+
+
+        //회사에서 redis를 쓸수있는가...(일단 로컬로 진행중)
         RefreshToken refreshToken = RefreshToken.builder()
                 .id(authentication.getName()) //어느 유저의 리프레시 토큰인가
                 .refreshToken(tokenDto.getRefreshToken())//실제 리프레시 토큰
                 .expiration(tokenProvider.getRefreshTokenRemainExpiration())//만료시간
                 .build();
+
+
 
         // 5. redis에 토큰 저장
         refreshTokenRedisRepository.save(refreshToken);
@@ -154,18 +198,42 @@ public class AuthService {
 
 
     //가입
-    public void signUp(SignUpRequestDto signUpRequestDto){
+    public URI signUp(SignUpRequestDto signUpRequestDto){
+
+        /*
+        save가 안됩니다 -> 근데 id return은 잘되는데 데베에는 없음;;;
+         */
 
         //유저 가입
-        UserInfo newUser = signUpRequestDto.toUser(signUpRequestDto.getPhoneNumber(), signUpRequestDto.getUserName(), signUpRequestDto.getUserRole());
-        UserInfo u = userRepository.save(newUser);
+        UserInfo newUser = signUpRequestDto.toUser(signUpRequestDto.getPhoneNum(), signUpRequestDto.getUserName(), signUpRequestDto.getUserRole());
+        UserInfo u = userInfoRepository.save(newUser);
 
-//        return createUri(t.getId(), UriTypes.TEACHER);
+
+        return createUri(u.getId(), UriTypes.USER );
+
+    }
+
+
+    //새로 생성되거나 수정된 job의 id를 포함한 URI만들기
+    public URI createUri(Long id, UriTypes uriTypes){
+        URI uri = UriComponentsBuilder.newInstance()
+//                .scheme("https")
+//                .host("m-crew.iptime.org")
+//                .port(8001)
+                .scheme("http")
+                .host("localhost")
+                .port(8080)
+                .path("/api/"+ uriTypes.getTypeName() + "/" + id)
+                .build()
+                .toUri(); //UriComponents into URI
+
+        return uri;
 
     }
 
 
     //로그인된 유저에게 반환해주어야 하는 정보들
+    @Transactional
     public SignInResponseDto createSignInInfo(TokenDto tokenDto){
 
         AccessTokenInfo accessTokenInfo = AccessTokenInfo.builder()
@@ -175,12 +243,12 @@ public class AuthService {
                 .build();
 
 
-        UserInfo u = userRepository.findById(tokenDto.getUserId()).orElseThrow(() -> new NotExistException(ErrorCode.NOT_EXIST_STUDENT));
+        UserInfo u = userInfoRepository.findById(tokenDto.getUserId()).orElseThrow(() -> new NotExistException(ErrorCode.NOT_EXIST_STUDENT));
 
 
         List<ClassOnboardInfo> classOnboardInfos = new ArrayList<>();
 
-        if(u.getRole().equals(UserTypes.TEACHER.getTypeName())){
+        if(u.getUserRole().equals(UserTypes.TEACHER.getTypeName())){
             //선생님의 학급 리스트
             List<Classes> classes = classRepository.findByTeacherId(u.getId());
 
@@ -235,8 +303,8 @@ public class AuthService {
 
         UserOnboardInfo userOnboardInfo = UserOnboardInfo.builder()
                 .userId(u.getId())
-                .name(u.getName())
-                .role(u.getRole())
+                .userName(u.getUserName())
+                .userRole(u.getUserRole())
                 .classInfo(classOnboardInfos)
                 .build();
 
