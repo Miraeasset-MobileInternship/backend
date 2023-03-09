ca7bfdc1563f1c3b648085268b3166af027a7c9a
yujeong <dldbwjd24@gmail.com>
2023-03-09 13:30:01 +0900

    feat(security) : config 파일 추가

--- src/main/java/miraeassetmobile/backend/config/security/JwtSecurityConfig.java
+++ src/main/java/miraeassetmobile/backend/config/security/JwtSecurityConfig.java
@@ -18,7 +18,7 @@ public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurity
     // TokenProvider 를 주입받아서 JwtFilter 를 통해 Security 로직에 필터를 등록
     @Override
     public void configure(HttpSecurity http){
-        JwtAuthenticationFilter customFilter = new JwtAuthenticationFilter(tokenProvider,logoutAccessTokenRedisRepository);
+        JwtAuthenticationFilter customFilter = new JwtAuthenticationFilter(tokenProvider, logoutAccessTokenRedisRepository);
         http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
--- src/main/java/miraeassetmobile/backend/config/security/SecurityConfig.java
+++ src/main/java/miraeassetmobile/backend/config/security/SecurityConfig.java
@@ -5,52 +5,55 @@ import miraeassetmobile.backend.config.security.jwt.JwtAccessDeniedHandler;
 import miraeassetmobile.backend.config.security.jwt.JwtAuthenticationEntryPoint;
 import miraeassetmobile.backend.config.security.jwt.TokenProvider;
 import miraeassetmobile.backend.repository.redis.LogoutAccessTokenRedisRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
-@EnableWebSecurity //Security 관련 설정을 활성화 -> 기본 스프링 filterChain에 등록한다
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-
 
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final LogoutAccessTokenRedisRepository logoutAccessTokenRedisRepository;
 
-
-    //AUTH 없이 사용할 수 있는 URL목록
     private static final String[] AUTH_LIST = {
             "/v3/api-docs",
             "/swagger-resources/**",
             "/swagger-ui/**",
             "/webjars/**",
-            "/api/**",
-
+            "/api/auth/**",
     };
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
     @Override
-    public void configure(WebSecurity web){
+    public void configure(WebSecurity web) throws Exception {
         // ACL(access control list)에 url 추가
+        web.ignoring().antMatchers("/book/**"); // 문제 조회 test를 위해 추가
         web.ignoring().antMatchers("/swagger-ui/**",
-                "/swagger-resources/**", "/v3/api-docs/**");
+                "/swagger-resources/**", "/v3/api-docs/**"); // debug시 swagger 사용을 위해 추가
     }
 
     protected void configure(HttpSecurity http) throws Exception {
 
         // csrf로 인한 forbidden error 방지
-
-        http.csrf().disable()//REST API 서버 CSRF처리 해제
-                .exceptionHandling()//예외처리 수행
-//                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class) // Custom Jwt 토큰 필터가 있는경우
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)//인증(사용자 신원 검증) 실패시
-                .accessDeniedHandler(jwtAccessDeniedHandler)//인가(인증 후에 자원 접근 프로세스) 실패시
+        http.csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
 
                 // 시큐리티는 기본적으로 세션을 통해 유저 정보들을 저장
                 // 하지만 redis를 사용하므로 세션을 사용하지 않아 세션 설정을 Stateless 로 설정
@@ -61,18 +64,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 인증없이 접근 가능한 url을 설정, AUTH_LIST에 추가하면 사용 가능.
                 .and()
                 .authorizeRequests()
+//                .antMatchers("/api/auth").permitAll()
                 .antMatchers(AUTH_LIST).permitAll()
                 .anyRequest().authenticated()
 
-
-
-
-
                 .and()
                 .apply(new JwtSecurityConfig(tokenProvider, logoutAccessTokenRedisRepository));
-
     }
-
-
-
 }
