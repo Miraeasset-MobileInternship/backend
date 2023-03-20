f31d3c8ab3abe0e7019378f3dcfc98f7698e9783
yujeong <dldbwjd24@gmail.com>
2023-03-20 11:17:41 +0900

    feat(자동 완성 검색) : autocomplete yhfinance API 연동

--- src/main/java/miraeassetmobile/backend/service/YhFinanceApiService.java
+++ src/main/java/miraeassetmobile/backend/service/YhFinanceApiService.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import miraeassetmobile.backend.domain.dto.api.yahooFinance.FinanceQuote;
-import miraeassetmobile.backend.domain.dto.api.yahooFinance.Symbol;
-import miraeassetmobile.backend.domain.dto.api.yahooFinance.TrendingByRegion;
-import miraeassetmobile.backend.domain.dto.api.yahooFinance.YahooFinanceUtils;
+import miraeassetmobile.backend.domain.dto.api.yahooFinance.*;
 import miraeassetmobile.backend.error.exception.ErrorCode;
 import miraeassetmobile.backend.error.exception.ServiceException;
 import org.springframework.http.ResponseEntity;
@@ -210,6 +207,93 @@ public class YhFinanceApiService {
         return new TrendingByRegion();
 
     }
+
+
+
+
+    //자동 완성 검색
+    public List<AutoComplete> getAutoComplete(String query) {
+
+        try {
+
+
+            StringBuilder urlBuilder = new StringBuilder(yahooFinanceUtils.getBaseUrl()+"/v6/finance/autocomplete"); /*URL*/
+            urlBuilder.append("?" + URLEncoder.encode("region", "UTF-8") + "=" + URLEncoder.encode("US", "UTF-8")); /*한 페이지 결과 수*/
+            urlBuilder.append("&" + URLEncoder.encode("lang", "UTF-8") + "=" + URLEncoder.encode("en", "UTF-8")); /*페이지 번호*/
+            urlBuilder.append("&" + URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(query, "UTF-8")); /*결과 형식*/
+
+
+
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("X-API-KEY", yahooFinanceUtils.getApiKey());
+
+            BufferedReader rd;
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                responseService.errorFromExternalServer();
+            }
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+
+            
+
+            //string to JSON
+            JSONObject jsonObject = new JSONObject(sb.toString());
+
+            JSONObject resultObject = jsonObject.getJSONObject("ResultSet"); //resultSet 제이슨 가져오기
+
+            JSONArray autoComList = resultObject.getJSONArray("Result");
+
+
+
+            List<AutoComplete> resultList = new ArrayList<>();
+
+            //결과가 여러개 일 수 있음
+            for(int i=0; i<autoComList.length(); i++){
+
+                JSONObject autoCom = autoComList.getJSONObject(i);
+
+                // ObjectMapper를 통해 String to Object로 변환
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.LOWER_CAMEL_CASE);
+
+                objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // NULL이 아닌 값만 응답받기(NULL인 경우는 생략)
+
+                AutoComplete auto = objectMapper.readValue(autoCom.toString(),
+                        new TypeReference<AutoComplete>() {
+                        });
+
+
+                resultList.add(auto);
+
+            }
+
+
+            return resultList;
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            responseService.errorFromExternalServerNoResult();
+        }
+
+        return new ArrayList<>();
+
+    }
+
+
 
 //
 //    public ResponseEntity getRealtimePrice(String symbol) {
