aac3cfcaa4c70fe9d489b014d8d1b87b3727fdc5
yujeong <dldbwjd24@gmail.com>
2023-03-23 10:14:29 +0900

    feat (stock-news) : 주식 뉴스 최신순 정렬 기능

---
+++ src/main/java/miraeassetmobile/backend/service/comparator/ListComparator.java
@@ -0,0 +1,16 @@
+package miraeassetmobile.backend.service.comparator;
+import miraeassetmobile.backend.domain.dto.api.rapidApiYhFinance.MarketNews;
+
+import java.util.Comparator;
+
+public class ListComparator implements Comparator {
+
+    //신문기사 최신순으로 정렬
+    @Override
+    public int compare(Object o1, Object o2) {
+        String date1 = ((MarketNews) o1).getPubDate();
+        String date2 = ((MarketNews) o2).getPubDate();
+
+        return date2.compareTo(date1);
+    }
+}
