c598b19706ece62c7e60362c521bb74384490190
yujeong <dldbwjd24@gmail.com>
2023-03-24 17:03:52 +0900

    feat : trending 변동 (스케쥴러)

--- src/main/java/miraeassetmobile/backend/BackendApplication.java
+++ src/main/java/miraeassetmobile/backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 
@@ -12,6 +13,7 @@ import java.util.Locale;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling //spring scheduler
 public class BackendApplication {
 
 	public static void main(String[] args) {
