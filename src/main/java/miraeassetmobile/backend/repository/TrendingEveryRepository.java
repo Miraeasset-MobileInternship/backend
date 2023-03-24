c154b8fdf5c08bead528fa03456538af1bfe67f8
yujeong <dldbwjd24@gmail.com>
2023-03-24 17:10:07 +0900

    feat( scheduler)

--- src/main/java/miraeassetmobile/backend/repository/TrendingEveryRepository.java
+++ src/main/java/miraeassetmobile/backend/repository/TrendingEveryRepository.java
@@ -1,2 +1,13 @@
-package miraeassetmobile.backend.repository;public interface TrendingEveryRepository {
+package miraeassetmobile.backend.repository;
+
+import miraeassetmobile.backend.domain.entity.TrendingEvery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface TrendingEveryRepository extends JpaRepository<TrendingEvery,Long> {
+
+    Optional<TrendingEvery> findTop1ByOrderByCreateTimestampDesc();
+
 }
