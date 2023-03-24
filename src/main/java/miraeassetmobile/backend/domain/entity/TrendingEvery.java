e3165eb38ba02e01aa1b80e918ccaf580e53a9c9
yujeong <dldbwjd24@gmail.com>
2023-03-24 17:10:16 +0900

    feat : scheduler

--- src/main/java/miraeassetmobile/backend/domain/entity/TrendingEvery.java
+++ src/main/java/miraeassetmobile/backend/domain/entity/TrendingEvery.java
@@ -1,2 +1,33 @@
-package miraeassetmobile.backend.domain.entity;public class TrendingEvery {
+package miraeassetmobile.backend.domain.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="trending_every")
+@Getter
+@NoArgsConstructor
+public class TrendingEvery extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //JPA 사용시 필요)
+    private Long id;
+
+
+
+    @Column(name = "data_string")
+    private String dataString;
+
+
+
+    @Builder
+    TrendingEvery(String dataString){
+        this.dataString = dataString;
+    }
+
 }
